437f1fa7d6d5ac8cbcdc3de5eaa8728fecb46c38
Code hero <87934576+dlwoals256@users.noreply.github.com>
2026-08-18 11:38:45 +0900

    [Passed] 상품 코드 (50ms, 16MB)

---
+++ 260818/상품 코드/product-code.py
@@ -0,0 +1,13 @@
+product_name, product_code = input().split()
+product_code = int(product_code)
+
+class Product:
+    def __init__(self, name="codetree", code=50):
+        self.name = name
+        self.code = code
+
+product1 = Product()
+product2 = Product(product_name, product_code)
+
+print("product", product1.code, "is", product1.name)
+print("product", product2.code, "is", product2.name)
