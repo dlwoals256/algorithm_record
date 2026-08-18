ec233ca982b62b0c28d96bc497ca8f1ac062171c
Code hero <87934576+dlwoals256@users.noreply.github.com>
2026-08-18 11:06:53 +0900

    [Passed] 007 (49ms, 16MB)

---
+++ 260818/007/007.py
@@ -0,0 +1,14 @@
+secret_code, meeting_point, time = input().split()
+time = int(time)
+
+class Solution:
+    def __init__(self, code, point, time):
+        self.code = secret_code
+        self.point = meeting_point
+        self.time = time
+
+solution = Solution(secret_code, meeting_point, time)
+
+print("secret code :", solution.code)
+print("meeting point :", solution.point)
+print("time :", solution.time)
